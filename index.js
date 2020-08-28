35794599f37af79a877a2c5cc04b4ab33f65c0e2
Shwinee <justecca2003@gmail.com>
2020-08-27 22:44:37 -0500

    Chat App 0.1

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
+const { request, response } = require('express');
 
-app.listen(3000, () => console.log('running at 3000'));
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log('running at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
     console.log("request sent");
+    const data = request.body;
+    database.insert(data);
     console.log(request.body);
+    response.json({
+        status: 'message sent',
+        text: data
+    })
 });
+
+app.get('/api', (request, response) => {
+    database.find({}, (error, data) => {
+        response.json(data);
+    });
+})
